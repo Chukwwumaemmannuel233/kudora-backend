24fd8966af40fb941e75496952ac95911fb04541
Chukwuma <echukwuma561@gmail.com>
2025-06-22 17:16:05 +0100

    Added admin login route

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
 const waitlistRoutes = require("./routes/waitlist");
+const adminRoutes = require("./routes/admin"); // or wherever you saved it
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/admin", adminRoutes);
+
 
 // Base route
 app.get("/", (req, res) => {
